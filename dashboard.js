d5f38df39551786fc33407e222299a8627feab0e
Warren Brown <warrengbrn@gmail.com>
2020-01-06 10:21:26 -0500

    added icons to page and new searches hide elements until all functions have finished loading

--- dashboard.js
+++ dashboard.js
@@ -3,7 +3,7 @@ var userHistory = [];
 var latitude;
 var longitude;
 var inputID;
-var apikey = 'YOUR_API_KEY';
+var apikey = '27238d939c7ff93d4301594b1144a712';
 
 //click search button
 $('#userInputButton').click(function (){
@@ -19,6 +19,9 @@ $('#userInputField').on('keydown', function(event){
 
 //search database function
 function searchDatabase(){
+
+//hides visibility of information until all elements loaded
+document.getElementById('initiallyHidden').style.visibility = "hidden";
 
 //Save user search history to array
 userInput = document.getElementById('userInputField').value; 
@@ -46,11 +49,18 @@ $.ajax({
     document.getElementById('currentTemperatureDiv').innerHTML = "Temperature: " + response.main.temp + " °F";
     document.getElementById('currentHumidityDiv').innerHTML = "Humidity: " + response.main.humidity + "%";
     document.getElementById('currentWindSpeed').innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
+    var iconcode = response.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $('#wicon').attr('src', iconurl);
     latitude = response.coord.lon;
     longitude = response.coord.lat;
     inputID = response.id;
 
     //search for current UV Index
+
+    //reset to blank before setting UV value in case of bad request
+    document.getElementById('currentUVIndex').innerHTML = "";
+
     $.ajax({
         url: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/uvi?',
     
@@ -62,8 +72,7 @@ $.ajax({
         })
         .then(function(response){
             console.log(response)
-            document.getElementById('currentUVIndexStartingText').innerHTML = "UV Index:";
-            document.getElementById('currentUVIndex').innerHTML = response.value;
+            document.getElementById('currentUVIndex').innerHTML = "UV Index: " + response.value;
 
             //search for 5 day forecast stats and unhide information
             $.ajax({
@@ -80,28 +89,47 @@ $.ajax({
                 console.log(response)
                 var currentDate = response.list[0].dt_txt.substr(0,10);
                 console.log(currentDate);
-                $('#currentDay').append(response.city.name + " ");
-                $('#currentDay').append(currentDate);
+                document.getElementById('currentDay').innerHTML = (response.city.name + " " + currentDate);
                 
                 document.getElementById('innerDate1').innerHTML = response.list[5].dt_txt.substr(0,10);
                 document.getElementById('innerTemp1').innerHTML = "Temp: " + response.list[5].main.temp + " °F";
                 document.getElementById('innerHumidity1').innerHTML = "Humidity: " + response.list[5].main.humidity + "%";
+                
+                var iconcode5day = response.list[5].weather[0].icon;
+                var iconurl5day = "http://openweathermap.org/img/w/" + iconcode5day + ".png";
+                $('#wicon1').attr('src', iconurl5day);
 
                 document.getElementById('innerDate2').innerHTML = response.list[13].dt_txt.substr(0,10);
                 document.getElementById('innerTemp2').innerHTML = "Temp: " + response.list[13].main.temp + " °F";
                 document.getElementById('innerHumidity2').innerHTML = "Humidity: " + response.list[13].main.humidity + "%";
 
+                var iconcode5day = response.list[13].weather[0].icon;
+                var iconurl5day = "http://openweathermap.org/img/w/" + iconcode5day + ".png";
+                $('#wicon2').attr('src', iconurl5day);
+
                 document.getElementById('innerDate3').innerHTML = response.list[21].dt_txt.substr(0,10);
                 document.getElementById('innerTemp3').innerHTML = "Temp: " + response.list[21].main.temp + " °F";
                 document.getElementById('innerHumidity3').innerHTML = "Humidity: " + response.list[21].main.humidity + "%";
+
+                var iconcode5day = response.list[21].weather[0].icon;
+                var iconurl5day = "http://openweathermap.org/img/w/" + iconcode5day + ".png";
+                $('#wicon3').attr('src', iconurl5day);
 
                 document.getElementById('innerDate4').innerHTML = response.list[29].dt_txt.substr(0,10);
                 document.getElementById('innerTemp4').innerHTML = "Temp: " + response.list[29].main.temp + " °F";
                 document.getElementById('innerHumidity4').innerHTML = "Humidity: " + response.list[29].main.humidity + "%";
 
+                var iconcode5day = response.list[29].weather[0].icon;
+                var iconurl5day = "http://openweathermap.org/img/w/" + iconcode5day + ".png";
+                $('#wicon4').attr('src', iconurl5day);
+
                 document.getElementById('innerDate5').innerHTML = response.list[37].dt_txt.substr(0,10);
                 document.getElementById('innerTemp5').innerHTML = "Temp: " + response.list[37].main.temp + " °F";
                 document.getElementById('innerHumidity5').innerHTML = "Humidity: " + response.list[37].main.humidity + "%";
+
+                var iconcode5day = response.list[37].weather[0].icon;
+                var iconurl5day = "http://openweathermap.org/img/w/" + iconcode5day + ".png";
+                $('#wicon5').attr('src', iconurl5day);
             
                 document.getElementById('initiallyHidden').style.visibility = "visible";
             })
